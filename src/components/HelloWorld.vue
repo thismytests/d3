f208b6936f12f50083abb8627e6c11d9fb49e803
Mykolai Lytvyn <frname.lsname@gmail.com>
2021-08-14 18:38:25 +0300

    Start

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,32 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div>
+    <h1>start</h1>
+    <div ref="d3-start"
+         class="d3-start"></div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  import * as d3 from 'd3'
+
+  function settingD3(d3El) {
+    const chart = d3.select(d3El);
+
+    chart.append('svg')
+      .append('rect')
+      .attr('width', 50)
+      .attr('height', 200)
+      .style('fill', 'red')
   }
-}
+
+  export default {
+    name: 'HelloWorld',
+    mounted() {
+      const d3El = this.$refs['d3-start'];
+      settingD3(d3El)
+    },
+    methods: {}
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
