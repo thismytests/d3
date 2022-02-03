6f8eeda36caf1399af90b3b9ac4dafbaf972da18
nikolay <thismytests@gmail.com>
2022-02-03 22:01:07 +0200

    In progress.

---
+++ mary.js
@@ -0,0 +1,98 @@
+// Техническая часть:
+// 1. Есть два класса: Company и Human
+// 2. Создать объекты: hr (Company), klaus (Human), maria (Human)
+
+// Логическая часть:
+// 1. Устроить Клауса на работу к HR
+// 2. Поженить Марию и Клауса
+// 3. Мария меняет телефон
+
+// Задача: когда супруг(а) работника меняет телефон, HR должен вывести “{Работник}: {новый телефон}”
+
+function simpleStringify (object){
+  var simpleObject = {};
+  for (var prop in object ){
+    if (!object.hasOwnProperty(prop)){
+      continue;
+    }
+    if (typeof(object[prop]) == 'object'){
+      continue;
+    }
+    if (typeof(object[prop]) == 'function'){
+      continue;
+    }
+    simpleObject[prop] = object[prop];
+  }
+  return JSON.stringify(simpleObject); // returns cleaned up JSON
+};
+
+class Company {
+  employees = [];
+
+  recruit(person) {
+    person.currentCompany = this
+    this.employees.push(person);
+  }
+
+  updateStaffPersonaInfo(person) {
+    // todo ... ask about this
+    // const info = `${person.name}: ${person.phoneNumber}`
+    const result = this.getPersonSpouse(person);
+    const info = `${result.name}: ${result.phoneNumber}`
+    console.log(info);
+  }
+
+  getPersonSpouse(person) {
+    return this.employees.find(item => item.name === person.name).secondHalf
+  }
+}
+
+class Human {
+  name = null;
+  secondHalf = null;
+  phoneNumber = null;
+  currentCompany = null;
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  marry(secondHalf) {
+    this.secondHalf = secondHalf;
+    this.secondHalf = JSON.parse(simpleStringify(secondHalf));
+    this.updatesInFamily();
+  }
+
+  buyPhone(phoneNumber) {
+    this.phoneNumber = phoneNumber
+    this.updatesInFamily()
+  }
+
+
+  updatesInFamily() {
+    if (this.secondHalf && this.secondHalf.currentCompany) {
+      this.secondHalf.currentCompany.updateStaffPersonaInfo(this.secondHalf)
+    }
+  }
+}
+
+const hr = new Company();
+const klaus = new Human('Klaus', false);
+const maria = new Human('Maria', false);
+
+
+// // 1. Устроить Клауса на работу к HR
+hr.recruit(klaus);
+
+
+// 2. Поженить Марию и Клауса
+klaus.marry(maria);
+// ????
+// klaus.updatesInFamily(klaus);
+maria.marry(klaus);
+
+
+maria.buyPhone("123")
+// 3. Мария меняет телефон
+maria.buyPhone("321")
+maria.buyPhone("1000")
